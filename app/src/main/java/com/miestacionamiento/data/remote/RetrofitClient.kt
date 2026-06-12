4ee2af167823017d5a8f91dba4a89f4cab06662f
LuisMonteroAndrade <lu.monteroandrade@gmail.com>
2026-06-11 23:59:28 -0400

    se modifico el RetrofitClient

--- app/src/main/java/com/miestacionamiento/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/miestacionamiento/data/remote/RetrofitClient.kt
@@ -8,13 +8,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    // Opciones de conexión (descomentar la que uses):
-    //   USB (adb reverse tcp:3000 tcp:3000): "http://10.0.2.2:3000/api/"
-    //   WiFi - cambiar X.X a tu IP local (ver con ipconfig): "http://192.168.X.X:3000/api/"
-    // private const val BASE_URL = "http://192.168.100.218:3000/api/"  // Lalo
-    // private const val BASE_URL = "http://10.0.2.2:3000/api/"         // USB/emulador
-
-    private const val BASE_URL = "http://192.168.1.35:3000/api/"
+        private const val BASE_URL = "http://161.153.192.177:3000/api/"
 
     val authInterceptor = AuthInterceptor()
 
